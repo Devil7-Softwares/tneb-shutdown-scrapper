818c7a0b85a9b97036fdac2904fbdd0ea5818df7
Devil7DK <dineshthangavel47@gmail.com>
2023-09-15 21:08:21 +0530

    Install and configure jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,22 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'jest/globals': true
   },
-  extends: ["standard-with-typescript"],
+  extends: ['standard-with-typescript'],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
+  overrides: [
+    {
+      files: ['**/*.test.ts'],
+      plugins: ['jest'],
+      extends: ['plugin:jest/recommended']
+    }
+  ],
   plugins: [],
-  rules: {},
+  rules: {
+    '@typescript-eslint/semi': 'off'
+  }
 };
